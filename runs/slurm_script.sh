a8ebe2a64f571028028af63cc3ee6f68f2038e85
herber4 <55756028+herber4@users.noreply.github.com>
2026-03-08 10:41:35 -0400

    Update slurm_script.sh

--- runs/slurm_script.sh
+++ runs/slurm_script.sh
@@ -1,1 +1,16 @@
 
+#!/bin/bash
+#SBATCH --job-name=cellranger
+#SBATCH -n 16
+#SBATCH --partition=compute
+#SBATCH --time=100:00:00
+#SBATCH --mem=128G
+#SBATCH --mail-type=all
+#SBATCH --output=/austin/pbmc/runs/cellranger.%j.out
+#SBATCH --error=/pbmc/runs/cellranger.%j.err
+#SBATCH --mail-user=herber4@clemson.edu
+# Load change dir
+
+cd /austin/pbmc/runs
+
+cellranger multi --localcores 16 --localmem 128 --disable-ui --id pbmc_slurm_test --csv ../multiconfig.csv
